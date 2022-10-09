ecbd3fceadcaa2ee834cde03a4d85f934147c4bc
Artyom Sosedka <sostema@gmail.com>
2022-10-09 03:13:39 +0300

    fix: proper key

--- src/anaserver/models/users_to_news.py
+++ src/anaserver/models/users_to_news.py
@@ -9,3 +9,4 @@ class UserToNews(Base):
     user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
     news_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("news.id"))
     action_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("actions.id"))
+    compound_key = sqlalchemy.PrimaryKeyConstraint(user_id, news_id, action_id)
