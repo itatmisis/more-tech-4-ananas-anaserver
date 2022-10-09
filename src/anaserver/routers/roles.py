99b536a8f869374e050a80883f8f96b464fb5e1e
komtriangle <komlevdanila742@gmail.com>
2022-10-09 09:11:45 +0300

    new routes

--- src/anaserver/routers/roles.py
+++ src/anaserver/routers/roles.py
@@ -16,6 +16,13 @@ router = APIRouter(
 @router.get("/", response_model=List[float])
 async def read_news(role_id: int) -> List[float]:
     async with get_session() as db:
-        return await crud.get_role_embedding(db, role_id);
+        return await crud.get_role_embedding(db, role_id)
+
+
+@router.get("/trend", response_model=schemas.News)
+async def trend_for_role(role_id:int) -> schemas.News:
+    db = get_session()
+    news_orm = await crud.get_trends_for_role(db, role_id)
+    return schemas.News.from_orm(news_orm)
 
 
