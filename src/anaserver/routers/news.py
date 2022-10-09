91fc971e5efb2273b95c962d7bc2e28f8bf23770
komtriangle <komlevdanila742@gmail.com>
2022-10-09 07:30:50 +0300

    async with

--- src/anaserver/routers/news.py
+++ src/anaserver/routers/news.py
@@ -15,7 +15,7 @@ router = APIRouter(
 
 @router.get("/", response_model=List[schemas.News])
 async def read_news() -> List[schemas.News]:
-    with get_session() as db:
+    async with get_session() as db:
         news_orm = await crud.get_all_news(db)
         news = [schemas.News.from_orm(news) for news in news_orm]
         return news
@@ -23,7 +23,7 @@ async def read_news() -> List[schemas.News]:
 
 @router.get("/{news_id}", response_model=schemas.News)
 async def read_news_by_id(news_id: UUID) -> schemas.News:
-    with get_session() as db:
+        async with get_session() as db:
         news_orm = await crud.get_news_by_id(db, news_id)
         news = schemas.News.from_orm(news_orm)
         return news
@@ -31,7 +31,7 @@ async def read_news_by_id(news_id: UUID) -> schemas.News:
 
 @router.get("/user/{user_id}", response_model=List[schemas.News])
 async def get_news_for_user(user_id: int) -> List[schemas.News]:
-    with get_session() as db:
+        async with get_session() as db:
         news_orm = await crud.get_news_for_user(db, user_id, 3)
         news = [schemas.News.from_orm(news) for news in news_orm]
         return news
@@ -39,7 +39,7 @@ async def get_news_for_user(user_id: int) -> List[schemas.News]:
 
 @router.get("/trends/{role_id}", response_model=List[schemas.News])
 async def get_trends_for_role(role_id: int) -> List[schemas.News]:
-    with get_session() as db:
+        async with get_session() as db:
         news_orm = await crud.get_trends_for_role(db, role_id)
         news = [schemas.News.from_orm(news) for news in news_orm]
         return news
--- src/anaserver/routers/users.py
+++ src/anaserver/routers/users.py
@@ -14,7 +14,7 @@ router = APIRouter(
 
 @router.get("/", response_model=List[schemas.User])
 async def read_users() -> List[schemas.User]:
-    with get_session() as db:
+        async with get_session() as db:
         users_orm = await crud.get_users(db)
         users = [schemas.User.from_orm(user) for user in users_orm]
         return users
@@ -22,7 +22,7 @@ async def read_users() -> List[schemas.User]:
 
 @router.post("/", response_model=schemas.User)
 async def create_user(user: schemas.UserCreate) -> schemas.User:
-    with get_session() as db:
+        async with get_session() as db:
         user_orm = models.User(id=user.id, role_id=user.role_id)
         result_orm = await crud.add_user(db, user_orm)
         result = schemas.User.from_orm(result_orm)
